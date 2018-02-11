fd8b0441c119d1d70690e1f1caa3a84d2d6191f6
rumamaheen <rumamaheen259@gmail.com>
2018-02-11 09:46:13 +0000

    Added string.c

--- string.c
+++ string.c
@@ -25,3 +25,14 @@ int main(void)
 
    return 0;
 }
+
+
+#include <stdio.h>
+-size_t my_strlen(char *s)
++size_t my_strlen(const char *s)
+{
+   - char *p = s;
+   + const char *p = s;
+   while (*p)
+   ++p;
+}
